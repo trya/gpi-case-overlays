cae2ee6da195d48dcc4a4dcc592e7c83e0002cdc
trya <tryagainprod@gmail.com>
2024-09-22 21:31:56 +0200

    Update switch_gpi_disp.sh

--- switch_gpi_disp.sh
+++ switch_gpi_disp.sh
@@ -15,7 +15,7 @@ if [ "$(cat /sys/class/drm/$device/status)" = "connected" ]; then
   while ! xrandr | grep "$external connected" >/dev/null; do sleep 0.1; done
   xrandr --output $external --auto --output $internal --off
   sudo -u $user pactl --server "$pserv" set-default-sink "alsa_output.platform-fef00700.hdmi.hdmi-stereo"
-elif [ "$(cat /sys/class/drm/${device}/status)" = "disconnected" ]; then
-  xrandr --output ${external} --off --output $internal --auto
+elif [ "$(cat /sys/class/drm/$device/status)" = "disconnected" ]; then
+  xrandr --output $external --off --output $internal --auto
   sudo -u $user pactl --server "$pserv" set-default-sink "alsa_output.usb-GeneralPlus_USB_Audio_Device-00.analog-stereo"
 fi
